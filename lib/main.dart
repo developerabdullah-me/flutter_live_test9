5620f59dc4852007796f5acb5814a85b4afbb390
MD ABDULLAH TALHA <abdullahtalha9999@gmail.comm>
2023-10-06 23:37:40 +0600

    last0

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:livetes9/HomeScreen.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: HomeScreen(),
-//     );
-//   }
-// }
-
 
 import 'package:flutter/material.dart';
 
